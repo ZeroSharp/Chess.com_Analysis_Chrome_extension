25f901845b1834f32336d9cee4ccddb6ed6a6a96
Zack Goldberg <zsgoldberg@gmail.com>
2019-04-02 21:25:09 -0400

    fix getPgn with async. adds extension for executing async functions

--- ext/src/bg/getpgn.js
+++ ext/src/bg/getpgn.js
@@ -1,13 +1,10 @@
-function getCurrentPgn() {
+async function getCurrentPgn() {
     var pgn = $('input[name="pgn"]').val();
     if (!pgn) {
-        var button = $('[id*="_ShareMenuGlobalDialogDownloadButton"')[0] || $(":button[ng-click='downloadPgn()']")[0];
-        button.click();
-        pgn = $('textarea[class="full"]').val();
-        var closeButton = $(':button[class="close"][id]').click();
-        closeButton.click();
+        pgn = await openShareDialog().then(openPgnTab).then(copyPgn);
+
     }
-    if (pgn) {    
+    if (pgn) {
         // The termination field confuses the PDF converter so the result
         // is often output as a draw. This replaces the content with "Normal"
         // which seems to work correctly.
@@ -17,7 +14,56 @@ function getCurrentPgn() {
             pgn = pgn.replace(/Termination "([^"]+)"/g, 'Termination "Normal"');
         }
     }
+    closeShareDialog();
     return pgn;
+}
+
+
+async function openPgnTab() {
+    var pgnTab = document.querySelector("#live_ShareMenuGlobalDialogDownloadButton");
+    if (!pgnTab) {
+        var headerElements = document.querySelectorAll(
+            ".share-menu-dialog-component header *") || document;
+        var pgnTab = Array.from(headerElements).filter(
+            (x) => x.textContent == "PGN")[0];
+    }
+    return new Promise((resolve, reject) => {
+        pgnTab.click();
+        setTimeout(resolve, 500);
+    });
+}
+
+function getShareButtonElement() {
+    var share
+}
+
+async function openShareDialog() {
+    var shareButton = document.querySelector('button.share-button-component.icon-share') ||
+        document.querySelector("#shareMenuButton");
+    if (shareButton) {
+        return new Promise((resolve, reject) => {
+            shareButton.click()
+            setTimeout(() => {
+                resolve();
+            }, 500);
+        });
+    } else {
+        return Promise.reject();
+    }
+}
+
+function closeShareDialog() {
+    var closeButton = $(".modal-container-component .x");
+    if (closeButton) {
+        closeButton.click();
+    }
+}
+
+function copyPgn() {
+    var textarea = document.querySelector(
+            "#live_ShareMenuPgnContentTextareaId") ||
+        document.querySelector("textarea[name=pgn]");
+    return textarea.value;
 }
 
 function popuptoast(message) {
